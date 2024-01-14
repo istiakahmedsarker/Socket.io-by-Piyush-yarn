089d99a0cae7570a0b85e4e37de7142f06e694ab
Istiak Ahmed Sarker <gamerestiak99@gmail.com>
2024-01-14 21:24:04 +0600

    public initListeners created

--- scalable-live-chat/apps/server/src/services/socket.ts
+++ scalable-live-chat/apps/server/src/services/socket.ts
@@ -7,6 +7,15 @@ class SocketService{
         this._io = new Server();
     }
 
+    public initListeners(){
+        const io = this.io;
+        io.on('connect',socket =>{
+            console.log(`New Socket Connected to ${socket.id}`);
+            socket.on('event:message', async({message}:{message:string})=>{
+                console.log("New Message received",message)
+            })
+        })
+    }
     get io(){
         return this._io;
     }
