70735a4e6199c0e6d49210d7998afd5c3d9b443b
Istiak Ahmed Sarker <gamerestiak99@gmail.com>
2024-01-16 20:41:41 +0600

    socket created

--- scalable-live-chat/apps/web/context/SocketProvider.tsx
+++ scalable-live-chat/apps/web/context/SocketProvider.tsx
@@ -1,40 +1,46 @@
 "use client"
-import React, { useCallback, useContext, useEffect } from 'react';
+import React, { useCallback, useContext, useEffect, useState } from 'react';
 import { createContext } from 'vm';
-import { io } from "socket.io-client";
+import { io, Socket } from "socket.io-client";
 
-interface SocketProviderProps{
-    children?:React.ReactNode;
+interface SocketProviderProps {
+    children?: React.ReactNode;
 }
 
-interface ISocketContext{
-    sendMessage:(msg:string)=>any;
+interface ISocketContext {
+    sendMessage: (msg: string) => any;
 }
 
 const SocketContext = React.createContext<ISocketContext | null>(null);
 
-export const useSocket = ()=>{
+export const useSocket = () => {
     const state = useContext(SocketContext);
-    if(!state) throw new Error(`state is undefined`);
+    if (!state) throw new Error(`state is undefined`);
     return state;
 }
 
-const SocketProvider: React.FC<SocketProviderProps> = ({children}) => {
-    const sendMessage: ISocketContext['sendMessage'] = useCallback((msg)=>{
+const SocketProvider: React.FC<SocketProviderProps> = ({ children }) => {
+    const [socket, setSocket] = useState<Socket>()
+
+    const sendMessage: ISocketContext['sendMessage'] = useCallback((msg) => {
         console.log("Send Message", msg);
-    },[])
-
-
-
-    useEffect(()=>{
-        const _socket = io('http://localhost:8000')
-
-        return ()=>{
-            _socket.disconnect();
+        if(socket){
+            socket.emit('sendMessage', msg)
         }
-    },[])
+    }, [])
+
+
+
+    useEffect(() => {
+        const _socket = io('http://localhost:8000')
+        setSocket(_socket)
+        return () => {
+            _socket.disconnect();
+            setSocket(undefined)
+        }
+    }, [])
     return (
-        <SocketContext.Provider value={{sendMessage}}>
+        <SocketContext.Provider value={{ sendMessage }}>
             {children}
         </SocketContext.Provider>
     );
